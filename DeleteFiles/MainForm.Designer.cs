cd062380965158dc0719107a98120782ec4b7274
ljf1239848066 <1239848066@qq.com>
2015-07-15 23:59:30 +0800

    init
    Signed-off-by: ljf1239848066 <1239848066@qq.com>

---
+++ DeleteFiles/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace 文件批量删除工具
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbDeletedList = new System.Windows.Forms.ListBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Location = new System.Drawing.Point(76, 11);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(293, 21);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "添加路径";
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl1.AutoEllipsis = true;
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(15, 140);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(134, 19);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "请添加要删除的路径...";
+            this.lbl1.UseCompatibleTextRendering = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(425, 11);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(42, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnScan.Location = new System.Drawing.Point(379, 11);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(40, 23);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "浏览";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.Recent;
+            // 
+            // lbDeletedList
+            // 
+            this.lbDeletedList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbDeletedList.FormattingEnabled = true;
+            this.lbDeletedList.ItemHeight = 12;
+            this.lbDeletedList.Location = new System.Drawing.Point(17, 40);
+            this.lbDeletedList.Name = "lbDeletedList";
+            this.lbDeletedList.Size = new System.Drawing.Size(450, 88);
+            this.lbDeletedList.TabIndex = 3;
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutomaticDelay = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 168);
+            this.Controls.Add(this.lbDeletedList);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1200, 700);
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件批量删除工具";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lbDeletedList;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ DeleteFiles/MainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace 文件批量删除工具
+{
+    public partial class MainForm : Form
+    {
+        private static int fileCount = 0;
+        private int dirCount = 0;
+        private string path = "";
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public void setPath(String path)
+        {
+            this.path = path;
+            tbPath.Text = path;
+        }
+
+        public void fun(string path)
+        {
+            try
+            {
+
+                String[] directoryNames = Directory.GetDirectories(path);//获取目录
+                String[] fileNames = Directory.GetFiles(path);//获取文件名
+                foreach (string file in fileNames)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                        fileCount++;
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        lbDeletedList.Items.Add(file.ToString());
+                        System.Windows.Forms.Application.DoEvents(); //必须加注这句代码，否则listBox1将因为循环执行太快而来不及显示信息
+                    }
+                }
+                foreach (string directory in directoryNames)
+                {
+                    fun(directory);
+                    try
+                    {
+                        Directory.Delete(directory);
+                        dirCount++;
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        lbDeletedList.Items.Add(directory.ToString());
+                        System.Windows.Forms.Application.DoEvents();
+                    }
+                }
+                Directory.Delete(path);
+                dirCount++;
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("确定要删除所有文件?", "提示", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                path = this.tbPath.Text;
+                if (Directory.Exists(path))
+                {
+                    Thread thread = new Thread(new ThreadStart(deleteThread));
+                    thread.Start();
+                }
+                else
+                    MessageBox.Show("此路径不存在!", "提示", MessageBoxButtons.OK);
+                btnScan.Focus();
+            }
+            else MessageBox.ReferenceEquals(null, null);
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+            path = this.tbPath.Text;
+            if (Directory.Exists(path))
+            {
+                btnDelete.Enabled = true;
+            }
+            else
+                btnDelete.Enabled = false;
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fBD = new FolderBrowserDialog();
+            if (fBD.ShowDialog() == DialogResult.Cancel)
+            {
+                btnDelete.Focus();
+                return;
+            }
+            else
+            {
+                this.tbPath.Text = fBD.SelectedPath;
+                lbl1.Text = "准备就绪...";
+            }
+            btnDelete.Focus();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Screen screen = Screen.PrimaryScreen;
+            this.Left = random.Next(screen.Bounds.Width-this.Width);
+            this.Top=random.Next(screen.Bounds.Height-this.Height);
+
+            Directory.CreateDirectory("C:/Users/Lenovo/Desktop/Test");
+            for (int i = 0; i < 100; i++)
+            {
+                string filename = "C:/Users/Lenovo/Desktop/Test/";
+                for (int k = 0; k < 10; k++)
+                {
+                    filename = "C:/Users/Lenovo/Desktop/Test/";
+                    for (int j = 0; j < i; j++)
+                        filename += "1";
+                    File.Create(filename + "a" + k + ".txt");
+                }
+            }
+
+        }
+
+        public void deleteThread()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            lbDeletedList.Items.Clear();
+            fileCount = 0;
+            dirCount = 0;
+            startTime = DateTime.Now;
+            btnDelete.Enabled = false;
+            lbl1.Text = "正在删除文件...";
+            fun(path);
+            btnDelete.Enabled = true;
+            endTime = DateTime.Now;
+            this.Text = "文件批量删除工具";
+            lbl1.Text = "删除成功 共删除" + fileCount + "个文件" + dirCount + "个文件夹 " + calculateTime(startTime, endTime);
+            string a = path;
+            this.tbPath.Text = path.Substring(0, path.LastIndexOf("\\"));
+            path = "";
+        }
+
+        public string calculateTime(DateTime start,DateTime end)
+        {
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            int millisecond = 0;
+            double totalMilliseconds = (end - start).TotalMilliseconds;
+            if (totalMilliseconds < 1000) return "用时" + totalMilliseconds + "毫秒";
+            else if (totalMilliseconds < 60 * 1000)
+            {
+                second = (int)(totalMilliseconds / 1000);
+                millisecond = (int)(totalMilliseconds % 1000);
+                return "用时" + second + "秒" + millisecond + "毫秒";
+            }
+            else if (totalMilliseconds < 60 * 1000 * 60)
+            {
+                minute = (int)(totalMilliseconds / 60000);
+                second = (int)((totalMilliseconds % 60000) / 1000);
+                millisecond = (int)(totalMilliseconds% 1000);
+                return "用时" + minute + "分" + second + "秒" + millisecond + "毫秒";
+            }
+            else if (totalMilliseconds < 60 * 1000 * 60 * 24)
+            {
+                hour = (int)(totalMilliseconds / 3600000);
+                minute = (int)((totalMilliseconds % 3600000)/60000);
+                second = (int)(((totalMilliseconds % 3600000) % 60000) / 1000);
+                millisecond = (int)(totalMilliseconds % 1000);
+                return "用时" + hour + "时" + minute + "分" + second + "秒" + millisecond + "毫秒";
+            }
+            else return "用时" + totalMilliseconds / 86400000 + "天";
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            this.lbl1.Top = this.Height - 60;
+            this.lbDeletedList.Height = this.Height - 112;
+            this.lbDeletedList.Width = this.Width - 50;
+            this.lbl1.Width = this.Width - 80;
+        }
+    }
+}
